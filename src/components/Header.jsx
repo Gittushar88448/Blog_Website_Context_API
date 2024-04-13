f52c4f53654f131428d43d7e8662e31ecebf371f
Tushar <kmrtushar01@gmail.com>
2024-04-13 11:03:36 +0530

    adding header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <header>
+            <h1>
+                Bloggerst
+            </h1>
+        </header>
+    </div>
   )
 }
 
