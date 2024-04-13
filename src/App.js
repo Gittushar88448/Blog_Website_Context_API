3ac7a70ca6d6743337d8f34eca31e6cdba778648
Tushar <kmrtushar01@gmail.com>
2024-04-13 10:59:55 +0530

    building components and calling the call api function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
 
-const App = () => {
+import './index.css';
+import Header from './components/Header';
+import Bottom from './components/Bottom';
+import MainPages from './components/MainPages';
+import { useContext, useEffect } from 'react';
+import  { AppContext }  from './context/AppContext';
+
+
+function App() {
+
+  const {fetchingPageData } = useContext(AppContext);
+
+
+  useEffect(() => {
+    fetchingPageData();
+  },[]);
+
+  
   return (
-    <div>App</div>
-  )
+    <div className="App">
+
+      <Header/>
+      <MainPages/>
+      <Bottom/>
+    </div>
+  );
 }
 
-export default App
+export default App;
