7f910501402b5e8d4ba7004ecc0ca4456af4ab0b
Tushar <kmrtushar01@gmail.com>
2024-04-13 11:02:50 +0530

    adding configuration of main blog page

--- src/components/MainPages.jsx
+++ src/components/MainPages.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import Spinner from './Spinner';
 
 const MainPages = () => {
+  const { loading, posts } = useContext(AppContext);
+  console.log(posts);
+
   return (
-    <div>MainPages</div>
+    <div>
+      {
+        loading ?
+
+          (<Spinner />) :
+
+          (
+            posts.length === 0 ?
+              (
+                <div>
+                  <p>Page not found</p>
+                </div>
+              ) :
+
+              (posts.map((post) => (
+                <div key={post.id}>
+                  <p className="font-bold text-lg ">{post.title}</p>
+                  <p className='text-sm mt-[4px]'>
+                    By <span className='italic'>{post.author}</span> on <span className='underline font-bold'>{post.category} </span>
+                  </p>
+                  <p className='text-sm mt-[4px]'>Posted on {post.date}</p>
+                  <p className='text-md mt-[14px]'>{post.content}</p>
+
+                  <div>
+                    {
+                      post.tags.map((tag , index) => (
+                        <span key={index}>
+                          {` #${tag}`}
+                        </span>
+                      ))
+                    }
+                  </div>
+
+                </div>
+              )))
+          )
+      }
+    </div>
   )
 }
 
