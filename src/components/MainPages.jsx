64fba3bdd6037801d82557f1910f4d639b59f73a
Tushar <kmrtushar01@gmail.com>
2024-04-14 19:41:45 +0530

    adding tailwind and light dark mode

--- src/components/MainPages.jsx
+++ src/components/MainPages.jsx
@@ -3,12 +3,14 @@ import { AppContext } from '../context/AppContext'
 import Spinner from './Spinner';
 
 const MainPages = () => {
-  const { loading, posts } = useContext(AppContext);
+  const { loading, posts, isDarkMode } = useContext(AppContext);
   console.log(posts);
 
-  return (
-    <div>
+  return (  
+    <div className={`w-11/12 max-w-[670px] py-8 flex flex-col gap-y-7 mt-[66px] mb-[50px] mx-auto bg-white
+      justify-center items-center ${isDarkMode ? "dark-mode" : "light-mode"}`}>
       {
+      
         loading ?
 
           (<Spinner />) :
@@ -16,13 +18,14 @@ const MainPages = () => {
           (
             posts.length === 0 ?
               (
-                <div>
+                <div className='flex justify-center items-center text-3xl font-bold '>
                   <p>Page not found</p>
                 </div>
               ) :
 
               (posts.map((post) => (
                 <div key={post.id}>
+
                   <p className="font-bold text-lg ">{post.title}</p>
                   <p className='text-sm mt-[4px]'>
                     By <span className='italic'>{post.author}</span> on <span className='underline font-bold'>{post.category} </span>
@@ -30,10 +33,10 @@ const MainPages = () => {
                   <p className='text-sm mt-[4px]'>Posted on {post.date}</p>
                   <p className='text-md mt-[14px]'>{post.content}</p>
 
-                  <div>
+                  <div className='flex gap-x-3'>
                     {
                       post.tags.map((tag , index) => (
-                        <span key={index}>
+                        <span key={index} className='text-blue-700 underline font-bold text-xs mt-[5px]'>
                           {` #${tag}`}
                         </span>
                       ))
@@ -45,6 +48,7 @@ const MainPages = () => {
           )
       }
     </div>
+
   )
 }
 
