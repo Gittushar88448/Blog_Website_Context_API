823845ac25519bf832dfb4aff28b92ebe5c63b2b
Tushar <kmrtushar01@gmail.com>
2024-04-15 23:15:05 +0530

    adding tailwind css to bottom section

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -3,31 +3,41 @@ import { AppContext } from '../context/AppContext'
 
 const Bottom = () => {
 
-  const {page , totalPages , handlePageChange} = useContext(AppContext);
+  const { page, totalPages, handlePageChange,isDarkMode } = useContext(AppContext);
 
   return (
-    <div>
-      { page > 1 &&
-        <button onClick={()=>{
-          handlePageChange(page-1);
-        }}>
-          Previous
-        </button>
-      }
-      {
-        page < 6 &&
-        <button onClick={()=>{
-          handlePageChange(page+1);
-        }}>
-          Next
-        </button>
-      }
-      {
-        <span>
-          {`pages ${page} of ${totalPages}`}
-        </span>
-      }
+     
+    <div className={`footer w-full flex justify-around items-center border-2 fixed bottom-0 bg-slate-200 ${isDarkMode ? "dark-mode" : "light-mode"}`}>
+      <div className='flex gap-x-2'>
+        {page > 1 &&
+          <button onClick={() => {
+            handlePageChange(page - 1);
+          }}
+          className='rounded-md border-2 px-4 py-1 bg-blue-500 text-white transition-all duration-300'
+          >
+            Previous
+          </button>
+        }
+        {
+          page < 6 &&
+          <button onClick={() => {
+            handlePageChange(page + 1);
+          }}
+          className='rounded-md border-2 px-4 py-1 bg-blue-500 text-white'
+          >
+            Next
+          </button>
+        }
+      </div>
+      <div>
+        {
+          <span className='font-bold text-sm'>
+            {`pages ${page} of ${totalPages}`}
+          </span>
+        }
+      </div>
     </div>
+
   )
 }
 
