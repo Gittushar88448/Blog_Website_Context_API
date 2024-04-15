5060baafb56c78104b78a6cec4431730d36ae449
Tushar <kmrtushar01@gmail.com>
2024-04-15 23:14:22 +0530

    adding darkmode light mode icon and tailwind css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { FaSun, FaMoon } from 'react-icons/fa';
+import { AppContext } from '../context/AppContext';
+
+
 
 const Header = () => {
+
+const {toggleDarkMode , isDarkMode} = useContext(AppContext);
+
   return (
     <div>
-        <header>
-            <h1>
-                Bloggerst
-            </h1>
-        </header>
+      <header className={`w-full border shadow-md py-4 fixed top-0 flex justify-center  bg-slate-200 ${isDarkMode ? "dark-mode" : "light-mode"}`}>
+        <h1 className='text-3xl text-center font-bold uppercase'>
+          Bloggerst
+        </h1>
+        <button onClick={()=> toggleDarkMode()} className='absolute right-[80px] top-[22px] w-8 '>
+          {
+            isDarkMode ? (<FaMoon className='w-8 h-6'/>) : (<FaSun className='w-8 h-6'/>)
+          }
+        </button>
+        
+      </header>
     </div>
   )
 }
