9105b9278764962d267c9aaac37907719fc57d74
Tushar <kmrtushar01@gmail.com>
2024-04-15 23:23:13 +0530

    importing  browserRouter from react-router-dom and wrap it in app component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import AppContextProvider from './context/AppContext';
-
+import BorwserRouter from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AppContextProvider>
-    <App />
-  </AppContextProvider>
+  <BorwserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BorwserRouter>
 );
