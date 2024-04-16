9591cbf72f51e570ff602f372bd3dea1ce069109
Tushar <kmrtushar01@gmail.com>
2024-04-16 12:42:42 +0530

    adding jsx code for blog page

--- src/components/Pages/Blogs.js
+++ src/components/Pages/Blogs.js
@@ -6,23 +6,23 @@ import Bottom from '../Bottom';
 import BlogDetails from '../BlogDetails';
 
 const Blogs = () => {
-    const [blog , setBlog] = useState(null);
-    const [relatedBlogs , setRelatedBlogs] = useState([]);
+    const [blog, setBlog] = useState(null);
+    const [relatedBlogs, setRelatedBlogs] = useState([]);
     const location = useLocation();
-    const {loading , setLoading} = useContext(AppContext);
+    const { loading, setLoading } = useContext(AppContext);
     const newBaseUrl = "https://codehelp-apis.vercel.app/api/";
     const navigate = useNavigate();
 
     const blogId = location.pathname.split("/").at(-1);
 
-    async function fetchRelatedBlogs(){
+    async function fetchRelatedBlogs() {
         setLoading(true);
-        try{
+        try {
             const response = await fetch(`${newBaseUrl}get-blog?blogId=${blogId}`);
             const data = await response.json();
             setBlog(data.blog);
             setRelatedBlogs(data.relatedBlogs);
-        }catch(e){
+        } catch (e) {
             console.log("fetching data error");
             setRelatedBlogs([]);
             setBlog(null);
@@ -30,15 +30,62 @@ const Blogs = () => {
         setLoading(false);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchRelatedBlogs();
-    },[location.pathname]);
+    }, [location.pathname]);
 
-  return (
-   <div>
-    Blog page
-   </div>
-  )
+    return (
+        <div>
+            <Header />
+            <div>
+                <button onClick={() => {
+                    navigate(-1);
+                }}>
+                    Back
+                </button>
+            </div>
+
+            {
+                loading ?
+                    (
+                        <div>
+                            <p>Loading...</p>
+                        </div>
+                    ) :
+
+                    blog ?
+                        (
+                            <div>
+
+                                <div>
+                                    <BlogDetails post={blog} />
+                                </div>
+
+                                <h2>Related Blogs</h2>
+
+                                {
+                                    relatedBlogs.map((post) => (
+                                        <div key={post.id}>
+                                            <BlogDetails post={post} />
+                                        </div>
+                                    ))
+                                }
+
+                            </div>
+
+                        ) :
+
+                        (
+                            <div>
+                                <p> No Data Found </p>
+                            </div>
+
+                        )
+
+            }
+
+        </div>
+    )
 }
 
 export default Blogs
