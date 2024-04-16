63fac6e452cfb8b053d3d4ba01c6d4d7832b97ba
Tushar <kmrtushar01@gmail.com>
2024-04-16 12:38:37 +0530

    adding home page component

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "../Header";
+import MainPages from "../MainPages";
+import Bottom from "../Bottom";
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <MainPages/>
+        <Bottom/>
+    </div>
+  )
+}
+
+export default Home
