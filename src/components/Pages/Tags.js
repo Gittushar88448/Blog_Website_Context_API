209d180983eb3b2bf4aa3348db09c449e6910be0
Tushar <kmrtushar01@gmail.com>
2024-04-17 17:29:30 +0530

    adding tailwind css to tag page

--- src/components/Pages/Tags.js
+++ src/components/Pages/Tags.js
@@ -14,13 +14,15 @@ const Tags = () => {
   return (
     <div>
         <Header/>
-        <div>
+        <div className='mt-[75px] w-11/12 max-w-[670px] items-center mx-auto flex gap-x-3 '>
             <button onClick={()=>{
                 navigate(-1);
-            }}>
+            }}
+            className='border font-bold px-4 py-2 bg-blue-500 rounded-md text-white'
+            >
                 Back
             </button>
-            <h2>
+            <h2 className='font-semibold flex gap-x-3'>
                 Blogs Tagged <span>#{tag}</span>
             </h2>
         </div>
