be66dcea8ca023e67de1616b7caba76230bcabd9
Tushar <kmrtushar01@gmail.com>
2024-04-17 17:27:12 +0530

    attaching blogDetails component using map

--- src/components/MainPages.jsx
+++ src/components/MainPages.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 import Spinner from './Spinner';
+import BlogDetails from './BlogDetails';
 
 const MainPages = () => {
   const { loading, posts, isDarkMode } = useContext(AppContext);
   console.log(posts);
 
   return (  
-    <div className={`w-11/12 max-w-[670px] py-8 flex flex-col gap-y-7 mt-[66px] mb-[50px] mx-auto bg-white
+    <div className={`w-11/12 max-w-[670px] py-8 flex flex-col gap-y-7 mt-[40px] mb-[50px] mx-auto bg-white
       justify-center items-center ${isDarkMode ? "dark-mode" : "light-mode"}`}>
       {
       
@@ -24,26 +25,7 @@ const MainPages = () => {
               ) :
 
               (posts.map((post) => (
-                <div key={post.id}>
-
-                  <p className="font-bold text-lg ">{post.title}</p>
-                  <p className='text-sm mt-[4px]'>
-                    By <span className='italic'>{post.author}</span> on <span className='underline font-bold'>{post.category} </span>
-                  </p>
-                  <p className='text-sm mt-[4px]'>Posted on {post.date}</p>
-                  <p className='text-md mt-[14px]'>{post.content}</p>
-
-                  <div className='flex gap-x-3'>
-                    {
-                      post.tags.map((tag , index) => (
-                        <span key={index} className='text-blue-700 underline font-bold text-xs mt-[5px]'>
-                          {` #${tag}`}
-                        </span>
-                      ))
-                    }
-                  </div>
-
-                </div>
+                <BlogDetails key={post.id} post={post}/>
               )))
           )
       }
